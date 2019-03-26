83dce8ea7ef3a1505a972c7db7afa2b1fe77f897
Student <example@example.com>
2019-03-25 17:23:33 -0700

    Fix identifier issues in index.js and index.html

--- src/api.js
+++ src/api.js
@@ -3,7 +3,8 @@ export class findDoctor {
     const Promise = require('es6-promise').Promise;
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=or-portland&sort=best-match-asc&100skip=2&limit=20&user_key=${process.env.exports.apiKey}`;
+      let url =
+      `https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
@@ -16,3 +17,5 @@ export class findDoctor {
     });
   };
 }
+
+// `https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=or-portland&sort=best-match-asc&skip=0&limit=20&user_key=${process.env.exports.apiKey}`;
--- src/index.js
+++ src/index.js
@@ -9,10 +9,10 @@ $(document).ready(function(){
   $("#findDoctors").click(function(event) {
     event.preventDefault();
 
-    const condition = $(".condition").val();
-    $(".condition").val();
-    const name = $(".name").val();
-    $(".name").val();
+    const condition = $("#condition").val();
+    $("#condition").val();
+    const name = $("#name").val();
+    $("#name").val();
 
     const findDoc = new findDoctor();
     const promise = findDoc.findByCondition(condition);
@@ -24,13 +24,11 @@ $(document).ready(function(){
         $(".output").append("<p>There are no doctors in the area that match the conditions.</p>");
       } else {
         for (let i = 0; i < 20; i++) {
-          console.log(body);
-
           const pic = body.data[i].profile.image_url;
           $(".output").append("<img src='" + pic + "'>");
 
           const firstName = body.data[i].profile.first_name;
-          const lastName = body.data[i].profile.first_name;
+          const lastName = body.data[i].profile.last_name;
           $(".output").append("<p> Name: " + firstName + " " + lastName + "</p>");
 
           const acceptPatients = body.data.practices[0].accepts_new_patients;
